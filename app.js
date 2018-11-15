1277c22fc80741bc473b4380b44496ae6640b0f4
Adam Zolyak <adam.zolyak@gmail.com>
2018-11-15 11:13:25 -0700

    wip epic status working

--- app.js
+++ app.js
@@ -6,48 +6,116 @@ axios.defaults.headers.common['Authorization'] = 'bearer ' + process.env.waffleA
 const waffleUserProjectsURL = `https://api.waffle.io/projects/5be5aab3b10da50134293eea/cards`
 
 const todaysDate = moment().format('YYYY-MM-DD')
-console.log("Today's date is: " + todaysDate)
-
 const todaysDateRaw = moment()
-console.log("Today's date is: " + todaysDateRaw)
 
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-
-        axios.get(waffleUserProjectsURL)
-        .then(response => {
-            
-            let issues = response.data
-
-            let openIssues = issues.filter(issue => Date.parse(issue.githubMetadata.closed_at) > (todaysDateRaw - 60480000))
-            console.log(openIssues)
-
-            let openIssuesHTML = ''
-
-            openIssues.forEach(issue => {
-                console.log(issue.githubMetadata.title)
-
-                openIssuesHTML += issue.githubMetadata.title + ', '
-
-                const issueDateClosed = Date.parse(issue.githubMetadata.closed_at)
-
-                console.log(todaysDateRaw - Date.parse(issue.githubMetadata.closed_at))
-            });
-
-            res.write(openIssuesHTML)
+    if (req.url === '/') {    
+        generateStatusReport() 
+        .then(statusReport => {
+            res.write(statusReport)
             res.end()
         })
-        .catch(error => {
-          console.log(error);
-          console.log("break 2")
-        });
+        
     } else {
         res.write('not found')
         res.end()
     }
 })
+
+async function getIssuesForProject() {
+    const response = await axios.get(waffleUserProjectsURL)
+    return response.data
+}
+
+function addEpicPropertyToIssues(issues) {
+    
+    issues.forEach(issue => {
+        let isEpic = issue.relationships.some(issueRelationship => {
+            let isParent = false
+            if (issueRelationship.relationship === 'parent') {
+                isParent = true
+            }
+            return isParent
+        })
+        if(isEpic) {
+            issue.isEpic = true
+        }
+    })
+}
+
+function addOrphanPropertyToIssues(issues) {
+    
+    issues.forEach(issue => {
+        let isEpic = issue.relationships.some(issueRelationship => {
+            let isParent = false
+            if (issueRelationship.relationship === 'parent') {
+                isParent = true
+            }
+            return isParent
+        })
+        if(isEpic) {
+            issue.isEpic = true
+        }
+    })
+}
+
+async function generateEpicStatus(epicIssues) {
+    let epicStatus = 'Epic Status Report'
+    
+    epicIssues.forEach(epic => {
+        epicStatus += '\n\n' + epic.githubMetadata.title + '\n'
+
+        epic.relationships.forEach(issueRelationship => {
+            if(issueRelationship.relationship === 'parent') {
+                epicStatus += `     "${issueRelationship.to.githubMetadata.title}" is ${issueRelationship.to.githubMetadata.state.toUpperCase()}\n`
+            }
+        })
+    })
+
+    return epicStatus
+}
+
+async function generateStatusReport() {
+            
+            let issues = await getIssuesForProject()
+
+            await addEpicPropertyToIssues(issues)
+
+            const epicIssues = issues.filter(issue => issue.isEpic === true);
+
+            const epicStatus = await generateEpicStatus(epicIssues)
+            console.log(epicStatus)
+
+            return epicStatus
+            
+            /*
+
+            let closedIssues = issues.filter(issue => Date.parse(issue.githubMetadata.closed_at) > (todaysDateRaw - (60 * 1000 * 60 * 24 * 7)))
+
+            let closedIssuesHTML = 'Closed Issues\n'
+
+            closedIssues.forEach(issue => {
+                closedIssuesHTML += issue.githubMetadata.title + '\n'
+            });
+
+            */
+
+            
+            
+
+}
+
+
+
+//Closed Issues
+
+// ... Epics + Child Issues
+
+// ... Unparented Issues
+
+//New Issues
 
 server.listen(3000)
 console.log('listening on port 3000...')
