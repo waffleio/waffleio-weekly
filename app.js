fbce60cfbb119ec49bdf04d182a15394999898af
Adam Zolyak <adam.zolyak@gmail.com>
2018-12-17 11:40:08 -0700

    wip

--- app.js
+++ app.js
@@ -1,169 +1,196 @@
-const issueHelpers = require("./issue");
-const issuesHelpers = require("./issues");
-const axios = require("axios");
-const moment = require("moment");
-const mailer = require("pug-mailer");
-const helmet = require("helmet");
-const express = require("express");
-const app = express();
-require("dotenv").config();
+const issueHelpers = require('./issue')
+const issuesHelpers = require('./issues')
+const axios = require('axios')
+const moment = require('moment')
+const mailer = require('pug-mailer')
+const helmet = require('helmet')
+const express = require('express')
+const app = express()
+require('dotenv').config()
 
-app.set("view engine", "pug");
-app.set("views", "./views");
+app.set('view engine', 'pug')
+app.set('views', './views')
 
-app.use(helmet());
+app.use(helmet())
 
 //config for your project
-const daysToReport = process.env.daysToReport;
-const webReportEnabled = process.env.webReportEnabled;
-const inProgressLabels = process.env.inProgressLabels.split(",");
+const daysToReport = process.env.daysToReport
+const webReportEnabled = process.env.webReportEnabled
+const inProgressLabels = process.env.inProgressLabels.split(',')
 
 mailer.init({
-  service: "Mailgun",
+  service: 'Mailgun',
   auth: {
     user: process.env.mailgunUser,
     pass: process.env.mailgunPass
   }
-});
+})
 
-const mailFrom = process.env.mailgunFrom;
-const mailTo = process.env.mailgunTo.split(",").join(",");
-console.log("Mail To: " + mailTo);
+const mailFrom = process.env.mailgunFrom
+const mailTo = process.env.mailgunTo.split(',').join(',')
+console.log('Mail To: ' + mailTo)
 
-const waffleProject = process.env.waffleProject;
+const waffleProject = process.env.waffleProject
 
-console.log(`Project: ${waffleProject}`);
-console.log(`Days to Report: ${daysToReport}`);
-console.log(`Web Report Enabled: ${webReportEnabled}`);
+console.log(`Project: ${waffleProject}`)
+console.log(`Days to Report: ${daysToReport}`)
+console.log(`Web Report Enabled: ${webReportEnabled}`)
 
-const todaysDateRaw = moment();
-const reportSinceDateRaw = todaysDateRaw - 60 * 1000 * 60 * 24 * daysToReport; // since 1 day ago
+const todaysDateRaw = moment()
+const reportSinceDateRaw = todaysDateRaw - 60 * 1000 * 60 * 24 * daysToReport // since 1 day ago
 
 const waffleAPI = axios.create({
-  baseURL: "https://api.waffle.io/",
+  baseURL: 'https://api.waffle.io/',
   timeout: 10000,
-  headers: { Authorization: "bearer " + process.env.waffleApiSecret }
-});
+  headers: { Authorization: 'bearer ' + process.env.waffleApiSecret }
+})
 
-async function getIssuesForProject(project) {
-  const url = `${project}/cards`;
+async function getReposForProject(project) {
+  const url = `projects/${project}`
 
   return await waffleAPI
     .get(url)
     .then(response => {
-      return response.data;
+      return response.data
     })
     .catch(error => {
       if (error.response) {
         // The request was made and the server responded with a status code
         // that falls out of the range of 2xx
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
+        console.log(error.response.data)
+        console.log(error.response.status)
+        console.log(error.response.headers)
       } else if (error.request) {
         // The request was made but no response was received
         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
         // http.ClientRequest in node.js
-        console.log(error.request);
-        return error.response.status;
+        console.log(error.request)
+        return error.response.status
       } else {
         // Something happened in setting up the request that triggered an Error
-        console.log("Error", error.message);
-        return 500;
+        console.log('Error', error.message)
+        return 500
       }
-    });
+    })
+}
+
+async function getIssuesForProject(project) {
+  const url = `${project}/cards`
+
+  return await waffleAPI
+    .get(url)
+    .then(response => {
+      return response.data
+    })
+    .catch(error => {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data)
+        console.log(error.response.status)
+        console.log(error.response.headers)
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request)
+        return error.response.status
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message)
+        return 500
+      }
+    })
 }
 
 async function ornamentIssuePrePruneMap(issue) {
   issue.isInProgress = await issueHelpers.checkIfInProgress(
     issue,
     inProgressLabels
-  );
-  issue.isChild = await issueHelpers.checkIfChild(issue);
+  )
+  issue.isChild = await issueHelpers.checkIfChild(issue)
 
-  return issue;
+  return issue
 }
 
 async function ornamentIssueMap(issue) {
-  issue.isEpic = await issueHelpers.checkIfEpic(issue);
-  issue.epics = await issueHelpers.getEpics(issue.relationships);
+  issue.isEpic = await issueHelpers.checkIfEpic(issue)
+  issue.epics = await issueHelpers.getEpics(issue.relationships)
   //issue.isInProgress = await issueHelpers.checkIfInProgress(issue, inProgressLabels)
   issue.currentState = await issueHelpers.getInProgressLabel(
     issue,
     inProgressLabels
-  );
-  issue.isPR = await issueHelpers.checkIfPR(issue);
-  issue.PRs = await issueHelpers.getPRs(issue.relationships);
+  )
+  issue.isPR = await issueHelpers.checkIfPR(issue)
+  issue.PRs = await issueHelpers.getPRs(issue.relationships)
   issue.assignees = await issueHelpers.getAssignees(
     issue.githubMetadata.assignees
-  );
+  )
 
   const issueDetail = await issuesHelpers.getIssueDetail(
     issue.githubMetadata.url
-  );
+  )
   if (issueDetail) {
-    issue.creator = issueDetail.user.login;
+    issue.creator = issueDetail.user.login
 
     const issueCommentsDetail = await issuesHelpers.getIssueCommentsDetail(
       issueDetail.comments_url
-    );
+    )
     if (issueCommentsDetail) {
       issue.newComments = await issueHelpers.getNewComments(
         issueCommentsDetail,
         reportSinceDateRaw
-      );
+      )
     } else {
-      issue.newComments = [];
+      issue.newComments = []
     }
 
     const issueEventsDetail = await issuesHelpers.getIssueEventsDetail(
       issueDetail.events_url
-    );
+    )
     if (issueEventsDetail) {
       issue.daysInCurrentState = issueHelpers.getDaysInState(
         issueEventsDetail,
         issue.currentState,
         moment()
-      );
+      )
     }
   }
 
-  return issue;
+  return issue
 }
 
 async function ornamentEpicMap(epic) {
-  let epicChildren = [];
+  let epicChildren = []
 
   epic.relationships.forEach(relationship => {
-    if (relationship.relationship === "parent") {
-      let issueSubset = Array.from(this.issues);
-      issueSubset = issueSubset.filter(
-        issue => relationship.to.id === issue.id
-      );
+    if (relationship.relationship === 'parent') {
+      let issueSubset = Array.from(this.issues)
+      issueSubset = issueSubset.filter(issue => relationship.to.id === issue.id)
 
       if (issueSubset.length == 1) {
-        epicChildren.push(issueSubset[0]);
+        epicChildren.push(issueSubset[0])
       }
     }
-  });
+  })
 
-  epic.childIssues = epicChildren;
+  epic.childIssues = epicChildren
 
-  return epic;
+  return epic
 }
 
 async function getReportData() {
-  let issues = await getIssuesForProject(waffleProject);
-  issues = await Promise.all(issues.map(ornamentIssuePrePruneMap));
-  issues = await issuesHelpers.pruneOldIssues(issues, reportSinceDateRaw);
-  issues = await Promise.all(issues.map(ornamentIssueMap));
+  let issues = await getIssuesForProject(waffleProject)
+  issues = await Promise.all(issues.map(ornamentIssuePrePruneMap))
+  issues = await issuesHelpers.pruneOldIssues(issues, reportSinceDateRaw)
+  issues = await Promise.all(issues.map(ornamentIssueMap))
 
-  let epicIssues = await issuesHelpers.getEpicIssues(issues);
-  epicIssues = await Promise.all(epicIssues.map(ornamentEpicMap, { issues }));
+  let epicIssues = await issuesHelpers.getEpicIssues(issues)
+  epicIssues = await Promise.all(epicIssues.map(ornamentEpicMap, { issues }))
 
   const reportData = {
-    title: "📈 Waffle.io Progress Report",
-    message: "📈 Waffle.io Progress Report",
+    title: '📈 Waffle.io Progress Report',
+    message: '📈 Waffle.io Progress Report',
     project: waffleProject,
     days: daysToReport,
     epics: epicIssues,
@@ -173,59 +200,143 @@ async function getReportData() {
       issues,
       reportSinceDateRaw
     )
-  };
+  }
 
-  return reportData;
+  return reportData
 }
 
-app.get("/", async (req, res) => {
-  const ghRateLimitStatus = await issuesHelpers.getGHApiRateLimitRemaining();
+async function getIssuesForProject() {
+  let repos = await getReposForProject(waffleProject)
+
+  let issues = []
+
+  for (source of repos.sources) {
+    const repo = source.repoPath
+    const state = 'closed'
+    const daysSince = 365
+    const issuesForRepo = await issuesHelpers.getIssuesForRepo(
+      repo,
+      state,
+      daysSince
+    )
+
+    for (issue of issuesForRepo) {
+      issues.push(issue)
+    }
+  }
+  return issues
+}
+
+function logLabelStats(issues, labelToMatch) {
+  const totalLabels = issues.length
+
+  const issuesWithLabel = issues.filter(issue => {
+    return issue.labels.some(label => {
+      return label.name === labelToMatch
+    })
+  })
+
+  const percentOfLabels = parseInt((issuesWithLabel.length / totalLabels) * 100)
+
+  console.log(
+    `${percentOfLabels}% ${labelToMatch} (${
+      issuesWithLabel.length
+    } of ${totalLabels} issues)`
+  )
+}
+
+async function getAllocationData() {
+  const issues = await getIssuesForProject()
+
+  const labels = [
+    'Eats',
+    'Adventures!',
+    'Family',
+    'Chores / Metawork',
+    'Friends / Relationships',
+    'Making',
+    'Wellness & Growth',
+    'Work',
+    'Truck Camper 🚚',
+    'JAI 🧠',
+    'BFAT! 🤟🤟🤟'
+  ]
+  labels.forEach(label => {
+    logLabelStats(issues, label)
+  })
+}
+
+app.get('/', async (req, res) => {
+  const ghRateLimitStatus = await issuesHelpers.getGHApiRateLimitRemaining()
 
   if (ghRateLimitStatus == 200) {
-    if (webReportEnabled === "true") {
-      const reportData = await getReportData();
+    if (webReportEnabled === 'true') {
+      const reportData = await getReportData()
 
-      res.status(200).render("report", reportData);
+      res.status(200).render('report', reportData)
 
-      issuesHelpers.printGHApiCountUsed();
+      issuesHelpers.printGHApiCountUsed()
     } else {
-      console.log("Web report is disabled.  Try email.");
-      res.status(200).send("Web report is disabled.  Try email.");
+      console.log('Web report is disabled.  Try email.')
+      res.status(200).send('Web report is disabled.  Try email.')
     }
   } else {
-    console.log(`Error: status ${ghRateLimitStatus}`);
+    console.log(`Error: status ${ghRateLimitStatus}`)
   }
-});
+})
 
-app.get("/email", async (req, res) => {
-  const ghRateLimitStatus = await issuesHelpers.getGHApiRateLimitRemaining();
+app.get('/allocation', async (req, res) => {
+  const ghRateLimitStatus = await issuesHelpers.getGHApiRateLimitRemaining()
 
   if (ghRateLimitStatus == 200) {
-    const reportData = await getReportData();
+    if (webReportEnabled === 'true') {
+      const reportData = await getAllocationData()
+
+      res.status(200)
+
+      return
+
+      res.status(200).render('report', reportData)
+
+      issuesHelpers.printGHApiCountUsed()
+    } else {
+      console.log('Web report is disabled.  Try email.')
+      res.status(200).send('Web report is disabled.  Try email.')
+    }
+  } else {
+    console.log(`Error: status ${ghRateLimitStatus}`)
+  }
+})
+
+app.get('/email', async (req, res) => {
+  const ghRateLimitStatus = await issuesHelpers.getGHApiRateLimitRemaining()
+
+  if (ghRateLimitStatus == 200) {
+    const reportData = await getReportData()
 
     mailer
       .send({
         from: mailFrom,
         to: mailTo,
         subject: `${reportData.title} for ${reportData.project}`,
-        template: __dirname + "/views/report.pug",
+        template: __dirname + '/views/report.pug',
         data: reportData
       })
       .then(response => {
-        console.log("Email Successfully Sent!");
-        res.status(200).send("Email Successfully Sent!");
+        console.log('Email Successfully Sent!')
+        res.status(200).send('Email Successfully Sent!')
       })
       .catch(err => {
-        console.log("Something went wrong!  Email not sent.");
-        console.log(err);
-        res.status(500).send("Something went wrong!  Email not sent.");
-      });
+        console.log('Something went wrong!  Email not sent.')
+        console.log(err)
+        res.status(500).send('Something went wrong!  Email not sent.')
+      })
 
-    issuesHelpers.printGHApiCountUsed();
+    issuesHelpers.printGHApiCountUsed()
   } else {
-    console.log(`Error: status ${ghRateLimitStatus}`);
+    console.log(`Error: status ${ghRateLimitStatus}`)
   }
-});
+})
 
-app.listen(3000);
-console.log("listening on port 3000...");
+app.listen(3000)
+console.log('listening on port 3000...')
--- issues.js
+++ issues.js
@@ -1,164 +1,189 @@
-const _ = require("lodash");
-const moment = require("moment");
-const axios = require("axios");
-const issueHelpers = require("./issue");
-require("dotenv").config();
+const _ = require('lodash')
+const moment = require('moment')
+const axios = require('axios')
+const octokit = require('@octokit/rest')()
+const issueHelpers = require('./issue')
+require('dotenv').config()
 
-let ghApiCount = 0;
+// token (https://github.com/settings/tokens)
+octokit.authenticate({
+  type: 'token',
+  token: process.env.ghApiToken
+})
+
+let ghApiCount = 0
 
 const ghAPI = axios.create({
   timeout: 10000,
-  headers: { Authorization: "token " + process.env.ghApiToken }
-});
+  headers: { Authorization: 'token ' + process.env.ghApiToken }
+})
 
 module.exports.printGHApiCountUsed = function() {
-  console.log("GH API Count: " + ghApiCount);
-};
+  console.log('GH API Count: ' + ghApiCount)
+}
 
 module.exports.getGHApiRateLimitRemaining = async function() {
-  const url = "https://api.github.com/rate_limit";
+  const url = 'https://api.github.com/rate_limit'
 
   return await ghAPI
     .get(url)
     .then(response => {
-      console.log(`GH API Remaining Calls: ${response.data.rate.remaining}`);
+      console.log(`GH API Remaining Calls: ${response.data.rate.remaining}`)
 
-      const timeNow = Date.parse(moment()) / 1000;
-      const timeReset = response.data.rate.reset;
-      console.log(`GH API Minutes to Reset: ${(timeReset - timeNow) / 60}`);
-      return response.status;
+      const timeNow = Date.parse(moment()) / 1000
+      const timeReset = response.data.rate.reset
+      console.log(`GH API Minutes to Reset: ${(timeReset - timeNow) / 60}`)
+      return response.status
     })
     .catch(error => {
       if (error.response) {
         // The request was made and the server responded with a status code
         // that falls out of the range of 2xx
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
+        console.log(error.response.data)
+        console.log(error.response.status)
+        console.log(error.response.headers)
       } else if (error.request) {
         // The request was made but no response was received
         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
         // http.ClientRequest in node.js
-        console.log(error.request);
-        return error.response.status;
+        console.log(error.request)
+        return error.response.status
       } else {
         // Something happened in setting up the request that triggered an Error
-        console.log("Error", error.message);
-        return 500;
+        console.log('Error', error.message)
+        return 500
       }
-    });
-};
+    })
+}
+
+module.exports.getIssuesForRepo = async function(repo, state, daysSince) {
+  ghApiCount++
+
+  const repoAndOwner = repo.split('/')
+  const since = moment().subtract(daysSince, 'days')
+
+  return await octokit
+    .paginate('GET /repos/:owner/:repo/issues', {
+      owner: repoAndOwner[0],
+      repo: repoAndOwner[1],
+      state: state,
+      since: since
+    })
+    .then(issues => {
+      return issues
+    })
+}
 
 module.exports.getIssueDetail = async function(url) {
-  ghApiCount++;
+  ghApiCount++
 
   return await ghAPI
     .get(url)
     .then(response => {
-      return response.data;
+      return response.data
     })
     .catch(error => {
-      console.log(`${error.message}(${url})`);
-    });
-};
+      console.log(`${error.message}(${url})`)
+    })
+}
 
 module.exports.getIssueEventsDetail = async function(url) {
-  ghApiCount++;
+  ghApiCount++
 
   return await ghAPI
     .get(url)
     .then(response => {
-      return response.data;
+      return response.data
     })
     .catch(error => {
-      console.log(`${error.message}(${url})`);
-    });
-};
+      console.log(`${error.message}(${url})`)
+    })
+}
 
 module.exports.getIssueCommentsDetail = async function(url) {
-  ghApiCount++;
+  ghApiCount++
 
   return await ghAPI
     .get(url)
     .then(response => {
-      return response.data;
+      return response.data
     })
     .catch(error => {
-      console.log(`${error.message}(${url})`);
-    });
-};
+      console.log(`${error.message}(${url})`)
+    })
+}
 
 module.exports.pruneOldIssues = function(issues, reportSinceDateRaw) {
   let issueSubset = issues.filter(
     issue =>
       Date.parse(issue.githubMetadata.created_at) > reportSinceDateRaw ||
       Date.parse(issue.githubMetadata.closed_at) > reportSinceDateRaw ||
-      issue.githubMetadata.state === "open"
-  );
+      issue.githubMetadata.state === 'open'
+  )
   issueSubset = issueSubset.filter(issue => {
     if (issue.isChild == true) {
-      return true;
-    } else if (issue.githubMetadata.state === "open") {
+      return true
+    } else if (issue.githubMetadata.state === 'open') {
       if (issue.isInProgress) {
-        return true;
+        return true
       } else {
-        return false;
+        return false
       }
     } else {
-      return true;
+      return true
     }
-  });
+  })
 
-  return issueSubset;
-};
+  return issueSubset
+}
 
 module.exports.getInProgressOrphanIssues = function(issues) {
   let issueSubset = issues.filter(
-    issue => issue.githubMetadata.state === "open"
-  );
-  issueSubset = issueSubset.filter(issue => issue.isInProgress === true);
-  issueSubset = issueSubset.filter(issue => issue.isEpic === false);
-  issueSubset = issueSubset.filter(issue => issue.isChild === false);
-  issueSubset = issueSubset.filter(issue => issue.isPR === false);
+    issue => issue.githubMetadata.state === 'open'
+  )
+  issueSubset = issueSubset.filter(issue => issue.isInProgress === true)
+  issueSubset = issueSubset.filter(issue => issue.isEpic === false)
+  issueSubset = issueSubset.filter(issue => issue.isChild === false)
+  issueSubset = issueSubset.filter(issue => issue.isPR === false)
 
-  issueSubset = _.orderBy(issueSubset, ["githubMetadata.updated_at"], ["asc"]);
+  issueSubset = _.orderBy(issueSubset, ['githubMetadata.updated_at'], ['asc'])
 
-  return issueSubset;
-};
+  return issueSubset
+}
 
 module.exports.getClosedIssues = function(issues, reportSinceDateRaw) {
   let issueSubset = issues.filter(
     issue => Date.parse(issue.githubMetadata.closed_at) > reportSinceDateRaw
-  );
+  )
   issueSubset = issueSubset.filter(
-    issue => issue.githubMetadata.state === "closed"
-  );
-  issueSubset = issueSubset.filter(issue => issue.isPR === false);
+    issue => issue.githubMetadata.state === 'closed'
+  )
+  issueSubset = issueSubset.filter(issue => issue.isPR === false)
 
-  issueSubset = _.orderBy(issueSubset, ["githubMetadata.closed_at"], ["asc"]);
+  issueSubset = _.orderBy(issueSubset, ['githubMetadata.closed_at'], ['asc'])
 
-  return issueSubset;
-};
+  return issueSubset
+}
 
 module.exports.getNewIssues = function(issues, reportSinceDateRaw) {
   let issueSubset = issues.filter(
     issue => Date.parse(issue.githubMetadata.created_at) > reportSinceDateRaw
-  );
-  issueSubset = issueSubset.filter(issue => issue.isPR === false);
+  )
+  issueSubset = issueSubset.filter(issue => issue.isPR === false)
 
   issueSubset = _.orderBy(
     issueSubset,
-    ["githubMetadata.state", "githubMetadata.created_at"],
-    ["desc", "asc"]
-  );
+    ['githubMetadata.state', 'githubMetadata.created_at'],
+    ['desc', 'asc']
+  )
 
-  return issueSubset;
-};
+  return issueSubset
+}
 
 module.exports.getEpicIssues = function(issues) {
-  let issueSubset = issues.filter(issue => issue.isEpic === true);
-  issueSubset = issueSubset.filter(issue => issue.isInProgress === true);
-  issueSubset = issueSubset.filter(issue => issue.isPR === false);
+  let issueSubset = issues.filter(issue => issue.isEpic === true)
+  issueSubset = issueSubset.filter(issue => issue.isInProgress === true)
+  issueSubset = issueSubset.filter(issue => issue.isPR === false)
 
-  return issueSubset;
-};
+  return issueSubset
+}
